f256a2a643614749dbff69b6b72f914a4f952a4f
Hunter Davis <hunterdavis730@gmail.com>
2024-05-28 23:39:28 -0600

    added some cool stuff

--- src/Views/PackageSelection/index.jsx
+++ src/Views/PackageSelection/index.jsx
@@ -5,7 +5,7 @@ import {
   TitleContainer,
   BaseCrmCardHeader,
   BaseCrmCardSubHeader,
-  BaseCrmCardFooter, 
+  BaseCrmCardFooter,
   PackageDetailsContainer,
   BaseCrmCardPackageDetailsHeader,
   BaseCrmCardPackageDetails,
@@ -25,116 +25,173 @@ import {
   UserTypeCardButton,
 } from "./styles";
 
-export const PackageSelection = () => {
+export const PackageSelection = ({
+  crmPackage,
+  setCrmPackage,
+  engageCard,
+  setEngageCard,
+  adminCount,
+  setAdminCount,
+  salesCount,
+  setSalesCount,
+  fieldCount,
+  setFieldCount,
+}) => {
+
   return (
     <PackageSelectionContainer>
-    <RowContainer>
-      <BaseCrmCard>
-        <TitleContainer>
+      <RowContainer>
+        <BaseCrmCard
+          onClick={() => setCrmPackage("growing")}
+          selected={crmPackage === "growing"}
+        >
+          <TitleContainer>
             <BaseCrmCardHeader>Growing</BaseCrmCardHeader>
             <BaseCrmCardSubHeader>Single-location Business</BaseCrmCardSubHeader>
-        </TitleContainer>
-        <PackageDetailsContainer>
+          </TitleContainer>
+          <PackageDetailsContainer>
             <BaseCrmCardPackageDetailsHeader>Limited Features</BaseCrmCardPackageDetailsHeader>
             <BaseCrmCardPackageDetails>Up to 5 integrations</BaseCrmCardPackageDetails>
-        </PackageDetailsContainer>
-        <BaseCrmCardFooter>Standard Onboarding</BaseCrmCardFooter>
-      </BaseCrmCard>
-      <BaseCrmCard>
-        <TitleContainer>
+          </PackageDetailsContainer>
+          <BaseCrmCardFooter>Standard Onboarding</BaseCrmCardFooter>
+        </BaseCrmCard>
+        <BaseCrmCard
+          onClick={() => setCrmPackage("established")}
+          selected={crmPackage === "established"}
+        >
+          <TitleContainer>
             <BaseCrmCardHeader>Established</BaseCrmCardHeader>
             <BaseCrmCardSubHeader>Run Multi-locations or Businesses</BaseCrmCardSubHeader>
-        </TitleContainer>
-        <PackageDetailsContainer>
+          </TitleContainer>
+          <PackageDetailsContainer>
             <BaseCrmCardPackageDetailsHeader>Features</BaseCrmCardPackageDetailsHeader>
             <BaseCrmCardPackageDetails>Unlimited integrations</BaseCrmCardPackageDetails>
             <BaseCrmCardPackageDetails>Access to Open API Documentation</BaseCrmCardPackageDetails>
-        </PackageDetailsContainer>
-        <BaseCrmCardFooter>Eligible for Account Manager $1000+</BaseCrmCardFooter>
-      </BaseCrmCard>
+          </PackageDetailsContainer>
+          <BaseCrmCardFooter>Eligible for Account Manager $1000+</BaseCrmCardFooter>
+        </BaseCrmCard>
       </RowContainer>
       <RowContainer>
-      <EngageCard>
-        <TitleContainer>
+        <EngageCard
+          onClick={() => setEngageCard("starter")}
+          selected={engageCard === "starter"}
+        >
+          <TitleContainer>
             <EngageCardHeader>Starter</EngageCardHeader>
             <EngageCardSubHeader>Ideal for up to 4 lines</EngageCardSubHeader>
-        </TitleContainer>
-        <PackageDetailsContainer>
-        <EngageCardPackageDetailsHeader>Features</EngageCardPackageDetailsHeader>
-        <EngageCardPackageDetails>2 phone lines included (up to 4)</EngageCardPackageDetails>
-        <EngageCardPackageDetails>Additional lines $4/month</EngageCardPackageDetails>
-        <EngageCardPackageDetails>200 credits included per month</EngageCardPackageDetails>
-        <EngageCardPackageDetails>$0.10/additional credit</EngageCardPackageDetails>
-        </PackageDetailsContainer>
-      </EngageCard>
-      <EngageCard>
-        <TitleContainer>
+          </TitleContainer>
+          <PackageDetailsContainer>
+            <EngageCardPackageDetailsHeader>Features</EngageCardPackageDetailsHeader>
+            <EngageCardPackageDetails>2 phone lines included (up to 4)</EngageCardPackageDetails>
+            <EngageCardPackageDetails>Additional lines $4/month</EngageCardPackageDetails>
+            <EngageCardPackageDetails>200 credits included per month</EngageCardPackageDetails>
+            <EngageCardPackageDetails>$0.10/additional credit</EngageCardPackageDetails>
+          </PackageDetailsContainer>
+        </EngageCard>
+        <EngageCard
+          onClick={() => setEngageCard("premium")}
+          selected={engageCard === "premium"}
+        >
+          <TitleContainer>
             <EngageCardHeader>Premium</EngageCardHeader>
             <EngageCardSubHeader>Ideal for 5-10 lines</EngageCardSubHeader>
-        </TitleContainer>
-        <PackageDetailsContainer>
-        <EngageCardPackageDetailsHeader>Features</EngageCardPackageDetailsHeader>
-        <EngageCardPackageDetails>10 phone lines included</EngageCardPackageDetails>
-        <EngageCardPackageDetails>Additional lines $4/month</EngageCardPackageDetails>
-        <EngageCardPackageDetails>2000 credits included per month</EngageCardPackageDetails>
-        <EngageCardPackageDetails>$0.035/additional credit</EngageCardPackageDetails>
-        </PackageDetailsContainer>
-      </EngageCard>
-      <EngageCard>
-        <TitleContainer>
+          </TitleContainer>
+          <PackageDetailsContainer>
+            <EngageCardPackageDetailsHeader>Features</EngageCardPackageDetailsHeader>
+            <EngageCardPackageDetails>10 phone lines included</EngageCardPackageDetails>
+            <EngageCardPackageDetails>Additional lines $4/month</EngageCardPackageDetails>
+            <EngageCardPackageDetails>2000 credits included per month</EngageCardPackageDetails>
+            <EngageCardPackageDetails>$0.035/additional credit</EngageCardPackageDetails>
+          </PackageDetailsContainer>
+        </EngageCard>
+        <EngageCard
+          onClick={() => setEngageCard("enterprise")}
+          selected={engageCard === "enterprise"}
+        >
+          <TitleContainer>
             <EngageCardHeader>Enterprise</EngageCardHeader>
             <EngageCardSubHeader>Ideal for up to 4 lines</EngageCardSubHeader>
-        </TitleContainer>
-        <PackageDetailsContainer>
-        <EngageCardPackageDetailsHeader>Features</EngageCardPackageDetailsHeader>
-        <EngageCardPackageDetails>25 phone lines included</EngageCardPackageDetails>
-        <EngageCardPackageDetails>Additional lines $4/month</EngageCardPackageDetails>
-        <EngageCardPackageDetails>10,000 credits included per month</EngageCardPackageDetails>
-        <EngageCardPackageDetails>$0.025/additional credit</EngageCardPackageDetails>
-        </PackageDetailsContainer>
-      </EngageCard>
+          </TitleContainer>
+          <PackageDetailsContainer>
+            <EngageCardPackageDetailsHeader>Features</EngageCardPackageDetailsHeader>
+            <EngageCardPackageDetails>25 phone lines included</EngageCardPackageDetails>
+            <EngageCardPackageDetails>Additional lines $4/month</EngageCardPackageDetails>
+            <EngageCardPackageDetails>10,000 credits included per month</EngageCardPackageDetails>
+            <EngageCardPackageDetails>$0.025/additional credit</EngageCardPackageDetails>
+          </PackageDetailsContainer>
+        </EngageCard>
       </RowContainer>
       <UserTypeSelectionContainer>
-        <UserTypeCard>
-            <UserTitleContainer>
-                <UserTypeCardHeader>Admin</UserTypeCardHeader>
-                <UserTypeCardSubHeader>All Access</UserTypeCardSubHeader>
-            </UserTitleContainer>
-            <UserTypeCounterContainer>
-                <UserTypeCounter>0</UserTypeCounter>
-                <UserTypeCardButtonContainer>
-                    <UserTypeCardButton>+</UserTypeCardButton>
-                    <UserTypeCardButton>-</UserTypeCardButton>
-                </UserTypeCardButtonContainer>
-            </UserTypeCounterContainer>
+        <UserTypeCard
+          selected={adminCount > 0}
+        >
+          <UserTitleContainer>
+            <UserTypeCardHeader>Admin</UserTypeCardHeader>
+            <UserTypeCardSubHeader>All Access</UserTypeCardSubHeader>
+          </UserTitleContainer>
+          <UserTypeCounterContainer>
+            <UserTypeCounter>{adminCount}</UserTypeCounter>
+            <UserTypeCardButtonContainer>
+              <UserTypeCardButton
+                onClick={() => setAdminCount(adminCount + 1)}
+              >
+                +
+              </UserTypeCardButton>
+              <UserTypeCardButton
+                onClick={() => adminCount > 0 && setAdminCount(adminCount - 1)}
+              >
+                -
+              </UserTypeCardButton>
+            </UserTypeCardButtonContainer>
+          </UserTypeCounterContainer>
         </UserTypeCard>
-        <UserTypeCard>
-            <UserTitleContainer>
-                <UserTypeCardHeader>Sales</UserTypeCardHeader>
-                <UserTypeCardSubHeader>Excludes Subscription<br></br>and Settings Access</UserTypeCardSubHeader>
-            </UserTitleContainer>
-            <UserTypeCounterContainer>
-                <UserTypeCounter>0</UserTypeCounter>
-                <UserTypeCardButtonContainer>
-                    <UserTypeCardButton>+</UserTypeCardButton>
-                    <UserTypeCardButton>-</UserTypeCardButton>
-                </UserTypeCardButtonContainer>
-            </UserTypeCounterContainer>
+        <UserTypeCard
+          selected={salesCount > 0}
+        >
+          <UserTitleContainer>
+            <UserTypeCardHeader>Sales</UserTypeCardHeader>
+            <UserTypeCardSubHeader>Excludes Subscription<br></br>and Settings Access</UserTypeCardSubHeader>
+          </UserTitleContainer>
+          <UserTypeCounterContainer>
+            <UserTypeCounter>{salesCount}</UserTypeCounter>
+            <UserTypeCardButtonContainer>
+              <UserTypeCardButton
+                onClick={() => setSalesCount(salesCount + 1)}
+              >
+                +
+              </UserTypeCardButton>
+              <UserTypeCardButton
+                onClick={() => salesCount > 0 && setSalesCount(salesCount - 1)}
+              >
+                -
+              </UserTypeCardButton>
+            </UserTypeCardButtonContainer>
+          </UserTypeCounterContainer>
         </UserTypeCard>
-        <UserTypeCard>
-            <UserTitleContainer>
-                <UserTypeCardHeader>Field</UserTypeCardHeader>
-                <UserTypeCardSubHeader>Mobile App Access Only<br></br>No Reporting Access</UserTypeCardSubHeader>
-            </UserTitleContainer>
-            <UserTypeCounterContainer>
-                <UserTypeCounter>0</UserTypeCounter>
-                <UserTypeCardButtonContainer>
-                    <UserTypeCardButton>+</UserTypeCardButton>
-                    <UserTypeCardButton>-</UserTypeCardButton>
-                </UserTypeCardButtonContainer>
-            </UserTypeCounterContainer>
+        <UserTypeCard
+          selected={fieldCount > 0}
+        >
+          <UserTitleContainer>
+            <UserTypeCardHeader>Field</UserTypeCardHeader>
+            <UserTypeCardSubHeader>Mobile App Access Only<br></br>No Reporting Access</UserTypeCardSubHeader>
+          </UserTitleContainer>
+          <UserTypeCounterContainer>
+            <UserTypeCounter>{fieldCount}</UserTypeCounter>
+            <UserTypeCardButtonContainer>
+              <UserTypeCardButton
+                onClick={() => setFieldCount(fieldCount + 1)}
+              >
+                +
+              </UserTypeCardButton>
+              <UserTypeCardButton
+                onClick={() => fieldCount > 0 && setFieldCount(fieldCount - 1)}
+              >
+                -
+              </UserTypeCardButton>
+            </UserTypeCardButtonContainer>
+          </UserTypeCounterContainer>
         </UserTypeCard>
-      </UserTypeSelectionContainer>    
+      </UserTypeSelectionContainer>
     </PackageSelectionContainer>
   );
 };
--- src/Views/pricingData.js
+++ src/Views/pricingData.js
@@ -1,38 +1,14 @@
 
-    // Pricing data for annual, 6-month, and monthly for the Growing and Established packages
-    // The pricing data is used to populate the pricing breakdown cards in the PricingSelection component
-    // The pricing data is also used to calculate the total cost of the package based on the user input
-    const pricingData = {
-        annual: {
-            growing: this.monthly.growing * 12,
-            established: this.monthly.established * 12,
-            starter: this.monthly.starter * 12,
-            premium: this.monthly.premium * 12,
-            enterprise: this.monthly.enterprise * 12,
-            admin: this.monthly.admin * 12,
-            sales: this.monthly.sales * 12,
-            field: this.monthly.field * 12,
-        },
-        sixMonth: {
-            growing: this.monthly.growing * 6,
-            established: this.monthly.established * 6,
-            starter: this.monthly.starter * 6,
-            premium: this.monthly.premium * 6,
-            enterprise: this.monthly.enterprise * 6,
-            admin: this.monthly.admin * 6,
-            sales: this.monthly.sales * 6,
-            field: this.monthly.field * 6,
-        },
-        monthly: {
-            growing: 199,
-            established: 499,
-            starter: 49,
-            premium: 99,
-            enterprise: 249,
-            admin: 75,
-            sales: 55,
-            field: 30,
-        },
-        }
+// Pricing data per unit
+const pricingData = {
+    growing: 199,
+    established: 499,
+    starter: 49,
+    premium: 99,
+    enterprise: 249,
+    admin: 75,
+    sales: 55,
+    field: 30,
+}
 
 export default pricingData;
